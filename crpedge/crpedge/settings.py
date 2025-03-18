04432380468a336abdfe9a991c106d6e79a3b771
Damodar <damodarpurohit29@gmail.com>
2025-03-19 00:02:56 +0530

    Initial commit

--- crpedge/crpedge/settings.py
+++ crpedge/crpedge/settings.py
@@ -24,8 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    'crp-backend.onrender.com',  # Add your Render domain here
+    '127.0.0.1',
+    'localhost',
+]
 
 # Application definition
 
