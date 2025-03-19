4d2e567309aa465caf2005a48f9a6c9ce939963f
Damodar <damodarpurohit29@gmail.com>
2025-03-19 11:52:11 +0530

    Initial commit

--- crpedge/crpedge/settings.py
+++ crpedge/crpedge/settings.py
@@ -161,7 +161,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Additional directories where Django looks for static files
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'crpedge', 'static'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
